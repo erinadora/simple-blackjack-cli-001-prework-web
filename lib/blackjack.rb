311f67220e3b5a00b9b8ab99444c6cbc4c60aa01
erinadora <eringoncer@gmail.com>
2016-01-25 20:56:37 -0500

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,44 +1,67 @@
-def welcome
-  # code #welcome here
+def runner
+  welcome
+  initial_round
+  cards = deal_card + deal_card
+    until cards > 21
+      hit?(cards)
+      cards += deal_card;
+    display_card_total(cards)
+  end
+  end_game(cards)
+end
+
+##################
+# shared methods #
+##################
+
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
 end
 
 def deal_card
-  # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+##########
+# rounds #
+##########
+
+def welcome
+  puts "Welcome to the Blackjack Table"
 end
 
 def prompt_user
-  # code #prompt_user here
-end
-
-def get_user_input
-  # code #get_user_input here
-end
-
-def end_game
-  # code #end_game here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def initial_round
-  # code #initial_round here
+  cards = deal_card + deal_card
+  display_card_total(cards)
+  return cards
 end
 
-def hit?
-  # code hit? here
+def hit?(card_total)
+    prompt_user
+    user_input = get_user_input
+  if user_input == "h"
+    card_total += deal_card;
+  elsif user_input == "s"
+    card_total
+  else
+    invalid_command
+  end
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Sorry, not a valid command"
+  hit?(card_total)
 end
 
-#####################################################
-# get every test to pass before coding runner below #
-#####################################################
-
-def runner
-  # code runner here
+def get_user_input
+  gets.chomp.strip
 end
-    
+
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
+end
+  
